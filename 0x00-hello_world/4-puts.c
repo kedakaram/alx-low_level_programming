6eeed6f8193cda130a58b8d905dfbedc9008e174
kedakaram <ahmedsaidwork12@gmail.com>
2023-03-18 06:34:45 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main (void){
-	puts("\"programing is like bulding a multilingual puzzle");
+int main(void){
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 
