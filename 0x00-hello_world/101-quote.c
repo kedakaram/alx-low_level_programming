35a741e0fbca2b2bc7e4cdb5b71ad5326340a307
kedakaram <ahmedsaidwork12@gmail.com>
2023-03-18 10:36:10 -0700

    h

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: print aqoute using writre function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ * Return: 1 (erorr).
+ */
+
+int main(void) 
+{       char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+        write (1, quo, 59);
+	return (1);
+}
