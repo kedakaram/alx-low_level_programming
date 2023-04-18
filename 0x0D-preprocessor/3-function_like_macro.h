f0d9b0d6ec7e4839d873bd0f63ef068e0d15cd5b
kedakaram <ahmedsaidwork12@gmail.com>
2023-04-18 13:50:04 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,6 @@
-#include <stdio.h>
+#ifndef _function_like_macro_h
+#define _function_like_macro_h
 
-/**
- * */
-int main(void)
-{
-	printf("%s\n", __FILE__);
-	return 0;
-}
+#define ABS(x) ((x) < 0 ?) -(X) : (x))
+
+#endif /* _function_like_macro_h */
