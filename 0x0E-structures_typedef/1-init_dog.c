de9d6dfe0635313af3f2e1db46346ff7e2de9855
kedakaram <ahmedsaidwork12@gmail.com>
2023-04-18 17:53:32 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,21 +2,21 @@
 #define DOG_H
 
 /**
- * dog structure representing a pet.
+ * struct dog - Represents a pet dog.
  * @name: dog's name.
  * @age: dog's age.
  * @owner: owner's name.
  *
- * Description: Defines a structure to represent a do
+ * Description: Defines a structure to represent a dog.
  */
 
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
